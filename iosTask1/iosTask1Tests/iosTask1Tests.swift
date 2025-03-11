72585493ea22b1007bb9fa4e03cee1425221a59f
Suyash <singhsuyash012@gmail.com>
2025-03-11 18:55:46 +0530

    Update iosTask1Tests.swift

--- iosTask1/iosTask1Tests/iosTask1Tests.swift
+++ iosTask1/iosTask1Tests/iosTask1Tests.swift
@@ -1,9 +1,3 @@
-//
-//  iosTask1Tests.swift
-//  iosTask1Tests
-//
-//  Created by Suyash Singh on 07/03/25.
-//
 
 import Testing
 @testable import iosTask1
@@ -11,7 +5,7 @@ import Testing
 struct iosTask1Tests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        
     }
 
 }
